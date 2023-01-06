c3407d8458b8c37549bf174fafbaeec5dc925f5a
DevYun97 <yundh812@gmail.com>
2023-01-06 22:19:53 +0900

    재도전제발

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
+module.exports = defineConfig({ 
+  outputDir: "../src/main/resources/static",  
+  indexPath: "../static/index.html",  
+  devServer: {  
+    port: 8081,
+    proxy: "http://localhost:8088"  
+  },  
+  chainWebpack: config => {  
+    const svgRule = config.module.rule("svg");    
+    svgRule.uses.clear();    
+    svgRule.use("vue-svg-loader").loader("vue-svg-loader");  
+  } 
 })
