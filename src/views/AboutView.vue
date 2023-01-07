f38f798869d614b25c633b28a212dea55fa10e53
DevYun97 <yundh812@gmail.com>
2023-01-07 18:56:21 +0900

    테스트커밋

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,23 +2,25 @@
   <div class="about">
     <h1>This is an about page</h1>
     <input type="text" v-model="testContent" />
-    <input type="button" @click="subDate" value="전송"/>
+    <input type="button" @click="subBtn" value="전송"/>
     <!-- <p>{{a}}</p> -->
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
+
   export default {
     name: 'about',
     data: () => ({
       testContent: ' '
     }),
     methods: {
-      subDate() {
+      subBtn() {
         const dd = new FormData();
         dd.append('demo', this.testContent)
-        this.axios.post('/test', dd)
-        .then(result =>{
+        this.axios.get('http://localhost:8088/test', dd)
+        .then(res =>{
           console.log(result.data)
         })
         .then((err)=>{
